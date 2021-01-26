9030075f1becacbed8e2b390902c0414b6f9bc46
hoyelam <hoyelam@protonmail.com>
2021-01-26 18:01:45 +0100

    Initial Commit

---
+++ Shared/Envision_Highlighter_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Envision_Highlighter_AssignmentApp.swift
+//  Shared
+//
+//  Created by Hoye Lam on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Envision_Highlighter_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
